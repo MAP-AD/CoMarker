cce3dc4a85021fb38bf9395f867c0cf06400fe4f
Samuel Boulger <111275286+slboulger@users.noreply.github.com>
2022-08-25 15:38:38 +0100

    Update coloc.R

--- Functions/coloc.R
+++ Functions/coloc.R
@@ -68,7 +68,7 @@ coloc<-function(image_directory,
   }
   
   ### get average of replicates
-  results=df %>%
+  results=na.omit(df) %>%
     group_by(CaseID,Slice)%>%
     dplyr::summarize(Mean_count = mean(Count), Mean_area=mean(Total.Area)) %>% 
     as.data.frame()
